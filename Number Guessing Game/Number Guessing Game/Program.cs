40269c41dca0e6988e41264ea9e4c9c9a14132f4
principyto <daniel.cerda@saei.mx>
2024-05-12 12:42:28 -0600

    Guessing game
    3th beginner project

---
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        int numberGuess;
+        int tries = 0;
+        Random random = new Random();
+
+        
+         Console.WriteLine("A number was generated, guess the number");
+         number = random.Next(1, 11);
+         Guess();
+         void Guess()
+         {
+              Console.WriteLine(tries);
+              Console.WriteLine("make a guess" + number);
+              numberGuess = Convert.ToInt32(Console.ReadLine());
+              if (numberGuess == number)
+              {
+                  Console.WriteLine("youre right, you win");
+                  Console.WriteLine("you got it in " + tries + " tries");
+                  return;
+              }
+              else if (numberGuess > number)
+              {
+                  Console.WriteLine("its a lower number");
+                  tries++;
+                  Guess();
+              }
+              else if (numberGuess < number)
+              {
+                  Console.WriteLine("its a higher number");
+                  tries++;
+                  Guess();
+              }
+         }
+    }
+}
+
